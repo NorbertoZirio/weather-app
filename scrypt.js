ed96e39527cd52ffaa8d4a6b3d940136cf6ebd95
NorbertoZirio <norbertozirio@gmail.com>
2025-07-30 19:08:49 -0700

    Actual conditions added

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,4 @@
-//const cityName = prompt("Ingresa una ciudad");
-
-async function getWeather(city) {
+async function getWeather(city = "La Habana") {
   const promes = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=GYNKRCLAP2XT62UF8J8ZLGCVH`,
     { mode: "cors" }
@@ -14,12 +12,38 @@ async function getWeather(city) {
 function procesData(object) {
   const weather = {
     tempCelcius: ((object.currentConditions.temp - 32) * 5) / 9,
+    maxTemp: ((object.days[0].tempmax - 32) * 5) / 9,
+    minTemp: ((object.days[0].tempmin - 32) * 5) / 9,
     conditions: object.currentConditions.conditions,
     humidity: object.currentConditions.humidity,
     precipprob: object.currentConditions.precipprob,
+    resolvedAddress: object.resolvedAddress,
   };
-
-  return console.log(weather.conditions);
+  updateScreen(weather);
+  //return console.log(weather.conditions);
 }
+getWeather();
 
-//getWeather(cityName);
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
+const tempH3 = document.querySelector("#temp-h3");
+const tempMaxH3 = document.querySelector("#maxtemp-h3");
+const tempMinH3 = document.querySelector("#mintemp-h3");
+const conditions = document.querySelector("#conditions");
+const precipprob = document.querySelector(`#precip-prob`);
+
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    getWeather(input.value);
+  }
+});
+
+function updateScreen(weather) {
+  h1.textContent = weather.resolvedAddress;
+  tempH3.textContent = `Actual temp: ${weather.tempCelcius.toFixed(2)}ºC`;
+  tempMaxH3.textContent = `Max temp: ${weather.maxTemp.toFixed(2)}ºC`;
+  tempMinH3.textContent = `Min temp: ${weather.minTemp.toFixed(2)}ºC`;
+  conditions.textContent = `Actual conditions: ${weather.conditions}`;
+  precipprob.textContent = `Humidity: ${weather.humidity}% Precipitation probabilty: ${weather.precipprob}%`;
+}
